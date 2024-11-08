a60b74daf4feb9fcda80074b861a020379059c3a
invictus-anic3tus <elijahdpalmer2010@gmail.com>
2024-11-08 15:45:44 -0600

    Fixed maybe

--- api/index.py
+++ api/index.py
@@ -1,17 +1,30 @@
-from g4f.client import Client
+# from g4f.client import Client
 
-from flask import Flask, render_template, request, jsonify
+# from flask import Flask, render_template, request, jsonify
+
+# app = Flask(__name__)
+
+
+# @app.route('/run-script', methods=['GET'])
+# # Create a button to submit the input
+# def run_script():
+#     client = Client()
+#     response = client.chat.completions.create(
+#         model="gpt-3.5-turbo",
+#         messages=[{"role": "user", "content": "Wassup"}],
+#         # Add any other necessary parameters
+#     )
+#     return jsonify({"message": response})
+
+
+
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/run-script', methods=['GET'])
-# Create a button to submit the input
 def run_script():
-    client = Client()
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": "Wassup"}],
-        # Add any other necessary parameters
-    )
-    return jsonify({"message": response})
+    # Replace this with the script you want to run
+    result = "Script has run successfully!"
+    return jsonify({"message": result})  # Ensures JSON response
